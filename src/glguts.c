ariahiro64/pj64-parallelrdp

commit 747adf6afb22bb71970b4ca9805a293a27647f98

Performance Improvements
Taken from https://github.com/loganmc10/parallel-rdp-standalone

src/glguts.c
@@ -25,14 +25,20 @@ bool window_widescreen;
 #define SHADER_HEADER "#version 330 core\n"
 #define TEX_FORMAT GL_RGBA
 #define TEX_TYPE GL_UNSIGNED_BYTE
+#define TEX_NUM 3
 
 static GLuint program;
 static GLuint vao;
-static GLuint texture;
+static GLuint buffer;
+static GLuint texture[TEX_NUM];
+static uint8_t *buffer_data;
+static uint32_t buffer_size = (640*8) * (480*8) * sizeof(uint32_t);
 
-int32_t tex_width;
-int32_t tex_height;
+int32_t tex_width[TEX_NUM];
+int32_t tex_height[TEX_NUM];
 static bool m_fullscreen;
+
+static int rotate_buffer;
 
 #define MSG_BUFFER_LEN 256
 
@@ -207,25 +213,27 @@ static GLuint gl_shader_link(GLuint vert, GLuint frag)
 
 bool screen_write(struct frame_buffer *fb)
 {
-    bool buffer_size_changed = tex_width != fb->width || tex_height != fb->height;
+    bool buffer_size_changed = tex_width[rotate_buffer] != fb->width || tex_height[rotate_buffer] != fb->height;
 
     // check if the framebuffer size has changed
     if (buffer_size_changed)
     {
-        tex_width = fb->width;
-        tex_height = fb->height;
+        tex_width[rotate_buffer] = fb->width;
+        tex_height[rotate_buffer] = fb->height;
         // set pitch for all unpacking operations
         glPixelStorei(GL_UNPACK_ROW_LENGTH, fb->pitch);
         // reallocate texture buffer on GPU
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, tex_width,
-                     tex_height, 0, TEX_FORMAT, TEX_TYPE, fb->pixels);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, tex_width[rotate_buffer],
+                     tex_height[rotate_buffer], 0, TEX_FORMAT, TEX_TYPE, fb->pixels);
     }
     else
     {
         // copy local buffer to GPU texture buffer
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width, tex_height,
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width[rotate_buffer], tex_height[rotate_buffer],
                         TEX_FORMAT, TEX_TYPE, fb->pixels);
     }
+
+    rotate_buffer = (rotate_buffer + 1) % TEX_NUM;
 
     return buffer_size_changed;
 }
@@ -264,45 +272,45 @@ void gl_screen_render()
 
     if(window_integerscale)
     {
-    unsigned width = win_width;
-	unsigned height = win_height;
-	int pad_x = 0, pad_y = 0;
-	unsigned base_h = 480;
-	unsigned base_w = 640;
-	if (width >= base_w && height >= base_h)
-	{
-		unsigned scale = min(width / base_w, height / base_h);
-		pad_x = width - base_w * scale;
-		pad_y = height - base_h * scale;
-	}
-	width -= pad_x;
-	height -= pad_y;
-	glViewport(pad_x / 2, pad_y / 2, width, height);
+        unsigned width = win_width;
+	    unsigned height = win_height;
+	    int pad_x = 0, pad_y = 0;
+	    unsigned base_h = 480;
+	    unsigned base_w = 640;
+	    if (width >= base_w && height >= base_h)
+	    {
+	    	unsigned scale = min(width / base_w, height / base_h);
+	    	pad_x = width - base_w * scale;
+	    	pad_y = height - base_h * scale;
+	    }
+	    width -= pad_x;
+	    height -= pad_y;
+	    glViewport(pad_x / 2, pad_y / 2, width, height);
     }
     else
     {
-    int32_t vp_x = 0;
-    int32_t vp_y = statusrect.bottom;
-    int display_width = 640 * vk_rescaling;
-    int display_height = 480 * vk_rescaling;
-    if(window_widescreen)
-    display_height = (480 * vk_rescaling)* 3 / 4;
-    int32_t hw =  display_height * win_width;
-    int32_t wh = display_width * win_height;
-
-    // add letterboxes or pillarboxes if the window has a different aspect ratio
-    // than the current display mode
-    if (hw > wh) {
-        int32_t w_max = wh / display_height;
-        vp_x += (win_width - w_max) / 2;
-        win_width = w_max;
-    } else if (hw < wh) {
-        int32_t h_max = hw / display_width;
-        vp_y += (win_height - h_max) / 2;
-        win_height = h_max;
-    }
-    // configure viewport
-    glViewport(vp_x, vp_y, win_width, win_height);
+        int32_t vp_x = 0;
+        int32_t vp_y = statusrect.bottom;
+        int display_width = 640 * vk_rescaling;
+        int display_height = 480 * vk_rescaling;
+        if(window_widescreen)
+        display_height = (480 * vk_rescaling)* 3 / 4;
+        int32_t hw =  display_height * win_width;
+        int32_t wh = display_width * win_height;
+    
+        // add letterboxes or pillarboxes if the window has a different aspect ratio
+        // than the current display mode
+        if (hw > wh) {
+            int32_t w_max = wh / display_height;
+            vp_x += (win_width - w_max) / 2;
+            win_width = w_max;
+        } else if (hw < wh) {
+            int32_t h_max = hw / display_width;
+            vp_y += (win_height - h_max) / 2;
+            win_height = h_max;
+        }
+        // configure viewport
+        glViewport(vp_x, vp_y, win_width, win_height);
     }
 
     
@@ -317,15 +325,18 @@ void gl_screen_clear(void)
 
 void gl_screen_close(void)
 {
-    tex_width = 0;
-    tex_height = 0;
-
-    glDeleteTextures(1, &texture);
+    glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+    glDeleteTextures(TEX_NUM, &texture[0]);
     glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &buffer);
     glDeleteProgram(program);
 }
 void screen_init()
 {
+    memset(tex_width, 0, sizeof(int32_t) * TEX_NUM);
+    memset(tex_height, 0, sizeof(int32_t) * TEX_NUM);
+    rotate_buffer = 0;
+
     if (gfx.hStatusBar)statusbar = gfx.hStatusBar;
     if(!statusbar)
     {
@@ -442,10 +453,18 @@ void screen_init()
     glBindVertexArray(vao);
 
     // prepare texture
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glGenTextures(TEX_NUM, &texture[0]);
+    for (int i = 0; i < TEX_NUM; ++i)
+    {
+        glBindTexture(GL_TEXTURE_2D, texture[i]);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    }
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, buffer);
+    glBufferStorage(GL_PIXEL_UNPACK_BUFFER, buffer_size * TEX_NUM, 0, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
+    buffer_data = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, buffer_size * TEX_NUM, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
 
     // check if there was an error when using any of the commands above
     gl_check_errors();
@@ -467,6 +486,11 @@ void screen_swap(bool blank)
 
    SwapBuffers(dc);
 
+}
+
+uint8_t* screen_get_texture_data()
+{
+    return buffer_data + (rotate_buffer * buffer_size);
 }
 
 void screen_toggle_fullscreen()

src/glguts.h
@@ -33,6 +33,7 @@ extern "C"
     void screen_toggle_fullscreen(void);
     void screen_close(void);
     void screen_swap(bool blank);
+    uint8_t* screen_get_texture_data();    
 
     extern int32_t window_width;
     extern int32_t window_height;

src/gl_core_3_3.c
@@ -97,6 +97,7 @@ void (CODEGEN_FUNCPTR *_ptrc_glBeginQuery)(GLenum target, GLuint id) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glBindBuffer)(GLenum target, GLuint buffer) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glBufferData)(GLenum target, GLsizeiptr size, const void * data, GLenum usage) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glBufferSubData)(GLenum target, GLintptr offset, GLsizeiptr size, const void * data) = NULL;
+void (CODEGEN_FUNCPTR *_ptrc_glBufferStorage)(GLenum target, GLsizeiptr size, const void * data, GLbitfield flags) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glDeleteBuffers)(GLsizei n, const GLuint * buffers) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glDeleteQueries)(GLsizei n, const GLuint * ids) = NULL;
 void (CODEGEN_FUNCPTR *_ptrc_glEndQuery)(GLenum target) = NULL;
@@ -538,6 +539,8 @@ static int Load_Version_3_3(void)
 	if(!_ptrc_glBufferData) numFailed++;
 	_ptrc_glBufferSubData = (void (CODEGEN_FUNCPTR *)(GLenum, GLintptr, GLsizeiptr, const void *))IntGetProcAddress("glBufferSubData");
 	if(!_ptrc_glBufferSubData) numFailed++;
+	_ptrc_glBufferStorage = (void (CODEGEN_FUNCPTR *)(GLenum, GLsizeiptr, const void *, GLbitfield))IntGetProcAddress("glBufferStorage");
+	if (!_ptrc_glBufferStorage) numFailed++;
 	_ptrc_glDeleteBuffers = (void (CODEGEN_FUNCPTR *)(GLsizei, const GLuint *))IntGetProcAddress("glDeleteBuffers");
 	if(!_ptrc_glDeleteBuffers) numFailed++;
 	_ptrc_glDeleteQueries = (void (CODEGEN_FUNCPTR *)(GLsizei, const GLuint *))IntGetProcAddress("glDeleteQueries");

src/gl_core_3_3.h
@@ -981,6 +981,9 @@ extern "C" {
 #define GL_TIME_ELAPSED 0x88BF
 #define GL_VERTEX_ATTRIB_ARRAY_DIVISOR 0x88FE
 
+#define GL_MAP_PERSISTENT_BIT 0x0040
+#define GL_MAP_COHERENT_BIT 0x0080
+
 extern void (CODEGEN_FUNCPTR *_ptrc_glBlendFunc)(GLenum sfactor, GLenum dfactor);
 #define glBlendFunc _ptrc_glBlendFunc
 extern void (CODEGEN_FUNCPTR *_ptrc_glClear)(GLbitfield mask);
@@ -1160,6 +1163,8 @@ extern void (CODEGEN_FUNCPTR *_ptrc_glBufferData)(GLenum target, GLsizeiptr size
 #define glBufferData _ptrc_glBufferData
 extern void (CODEGEN_FUNCPTR *_ptrc_glBufferSubData)(GLenum target, GLintptr offset, GLsizeiptr size, const void * data);
 #define glBufferSubData _ptrc_glBufferSubData
+extern void (CODEGEN_FUNCPTR *_ptrc_glBufferStorage)(GLenum target, GLsizeiptr size, const void * data, GLbitfield flags);
+#define glBufferStorage _ptrc_glBufferStorage
 extern void (CODEGEN_FUNCPTR *_ptrc_glDeleteBuffers)(GLsizei n, const GLuint * buffers);
 #define glDeleteBuffers _ptrc_glDeleteBuffers
 extern void (CODEGEN_FUNCPTR *_ptrc_glDeleteQueries)(GLsizei n, const GLuint * ids);

src/parallel_imp.cpp
@@ -3,7 +3,6 @@
 #include "parallel_imp.h"
 #include <memory>
 #include <vector>
-
 #include "rdp_device.hpp"
 #include "context.hpp"
 #include "device.hpp"
@@ -31,6 +30,7 @@ bool vk_native_tex_rect;
 bool vk_synchronous, vk_divot_filter, vk_gamma_dither;
 bool vk_vi_aa, vk_vi_scale, vk_dither_filter;
 bool vk_interlacing;
+bool skip_swap_clear;
 
 static const unsigned cmd_len_lut[64] = {
 	1, 1, 1, 1, 1, 1, 1, 1, 4, 6, 12, 14, 12, 14, 20, 22,
@@ -39,7 +39,7 @@ static const unsigned cmd_len_lut[64] = {
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,
 };
 
-void vk_blit(std::vector<RDP::RGBA> &colors, unsigned &width, unsigned &height)
+void vk_blit(unsigned &width, unsigned &height)
 {
 	if (running)
 	{
@@ -63,16 +63,15 @@ void vk_blit(std::vector<RDP::RGBA> &colors, unsigned &width, unsigned &height)
 		{
 			width = 0;
 			height = 0;
-			colors.clear();
 			return;
 		}
 
 		width = scanout.width;
 		height = scanout.height;
-		colors.resize(width * height);
 
 		scanout.fence->wait();
-		memcpy(colors.data(), device->map_host_buffer(*scanout.buffer, Vulkan::MEMORY_ACCESS_READ_BIT),
+		uint8_t* color_data = screen_get_texture_data();
+		memcpy(color_data, device->map_host_buffer(*scanout.buffer, Vulkan::MEMORY_ACCESS_READ_BIT),
 			   width * height * sizeof(uint32_t));
 		device->unmap_host_buffer(*scanout.buffer, Vulkan::MEMORY_ACCESS_READ_BIT);
 	}
@@ -98,21 +97,25 @@ void vk_rasterize()
 		frontend->set_vi_register(RDP::VIRegister::XScale, *GET_GFX_INFO(VI_X_SCALE_REG));
 		frontend->set_vi_register(RDP::VIRegister::YScale, *GET_GFX_INFO(VI_Y_SCALE_REG));
 
+		RDP::Quirks quirks;
+		quirks.set_native_texture_lod(vk_native_texture_lod);
+		quirks.set_native_resolution_tex_rect(vk_native_tex_rect);
+		frontend->set_quirks(quirks);
+
 		frontend->begin_frame_context();
 
 		unsigned width = 0;
 		unsigned height = 0;
-		std::vector<RDP::RGBA> cols;
-		vk_blit(cols, width, height);
+		vk_blit(width, height);
 
 		if (width == 0 || height == 0)
 		{
-			screen_swap(true);
+			if (!skip_swap_clear)
+				screen_swap(true);
 			return;
 		}
 
 		struct frame_buffer buf = {0};
-		buf.pixels = (video_pixel *)cols.data();
 		buf.valid = true;
 		buf.height = height;
 		buf.width = width;
@@ -129,7 +132,7 @@ void vk_process_commands()
 
 		const uint32_t DP_CURRENT = *GET_GFX_INFO(DPC_CURRENT_REG) & 0x00FFFFF8;
 		const uint32_t DP_END = *GET_GFX_INFO(DPC_END_REG) & 0x00FFFFF8;
-        *GET_GFX_INFO(DPC_STATUS_REG) &= ~DP_STATUS_FREEZE;
+
 		int length = DP_END - DP_CURRENT;
 		if (length <= 0)
 			return;
@@ -187,7 +190,7 @@ void vk_process_commands()
 			if (RDP::Op(command) == RDP::Op::SyncFull)
 			{
 				// For synchronous RDP:
-				if (frontend)
+				if (vk_synchronous && frontend)
 					frontend->wait_for_timeline(frontend->signal_timeline());
 				*gfx.MI_INTR_REG |= DP_INTERRUPT;
 				gfx.CheckInterrupts();
@@ -228,7 +231,8 @@ bool vk_init()
 	uintptr_t aligned_rdram = reinterpret_cast<uintptr_t>(gfx.RDRAM);
 	uintptr_t offset = 0;
 
-    if (device->get_device_features().supports_external_memory_host)
+
+	if (device->get_device_features().supports_external_memory_host)
 	{
 		size_t align = device->get_device_features().host_memory_properties.minImportedHostPointerAlignment;
 		offset = aligned_rdram & (align - 1);
@@ -241,7 +245,7 @@ bool vk_init()
 	}
 
 	device->set_context(*context);
-	device->init_frame_contexts(1);
+	device->init_frame_contexts(3);
 	::RDP::CommandProcessorFlags flags = 0;
 
 	switch (vk_rescaling)
@@ -261,7 +265,7 @@ bool vk_init()
 	default:
 		break;
 	}
-	if ((vk_rescaling > 1) && vk_ssreadbacks)
+	if (vk_rescaling >1 && vk_ssreadbacks)
 		flags |= RDP::COMMAND_PROCESSOR_FLAG_SUPER_SAMPLED_READ_BACK_BIT;
 	if (vk_ssdither)
 		flags |= RDP::COMMAND_PROCESSOR_FLAG_SUPER_SAMPLED_DITHER_BIT;
@@ -273,21 +277,12 @@ bool vk_init()
 		frontend.reset();
 		return false;
 	}
+
+	RDP::Quirks quirks;
+	quirks.set_native_texture_lod(vk_native_texture_lod);
+	quirks.set_native_resolution_tex_rect(vk_native_tex_rect);
+	frontend->set_quirks(quirks);
+
 	running = true;
 	return true;
-}
-
-static const VkApplicationInfo parallel_app_info = {
-	VK_STRUCTURE_TYPE_APPLICATION_INFO,
-	nullptr,
-	"paraLLEl-RDP",
-	0,
-	"Granite",
-	0,
-	VK_API_VERSION_1_1,
-};
-
-const VkApplicationInfo *parallel_get_application_info(void)
-{
-	return &parallel_app_info;
 }

commit 7af6083dc49ea6c3de3ef1769354e7bbe9ade96c

Fix issues caused by 747adf6afb22bb71970b4ca9805a293a27647f98
I missed a few tiny things from Logan's work

src/glguts.c
@@ -36,8 +36,8 @@ static uint32_t buffer_size = (640*8) * (480*8) * sizeof(uint32_t);
 
 int32_t tex_width[TEX_NUM];
 int32_t tex_height[TEX_NUM];
-static bool m_fullscreen;
 
+static bool m_fullscreen;
 static int rotate_buffer;
 
 #define MSG_BUFFER_LEN 256
@@ -211,9 +211,10 @@ static GLuint gl_shader_link(GLuint vert, GLuint frag)
     return program;
 }
 
-bool screen_write(struct frame_buffer *fb)
+void screen_write(struct frame_buffer *fb)
 {
     bool buffer_size_changed = tex_width[rotate_buffer] != fb->width || tex_height[rotate_buffer] != fb->height;
+    char* offset = rotate_buffer * buffer_size;
 
     // check if the framebuffer size has changed
     if (buffer_size_changed)
@@ -224,18 +225,16 @@ bool screen_write(struct frame_buffer *fb)
         glPixelStorei(GL_UNPACK_ROW_LENGTH, fb->pitch);
         // reallocate texture buffer on GPU
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, tex_width[rotate_buffer],
-                     tex_height[rotate_buffer], 0, TEX_FORMAT, TEX_TYPE, fb->pixels);
+                     tex_height[rotate_buffer], 0, TEX_FORMAT, TEX_TYPE, offset);
     }
     else
     {
         // copy local buffer to GPU texture buffer
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width[rotate_buffer], tex_height[rotate_buffer],
-                        TEX_FORMAT, TEX_TYPE, fb->pixels);
+                        TEX_FORMAT, TEX_TYPE, offset);
     }
 
     rotate_buffer = (rotate_buffer + 1) % TEX_NUM;
-
-    return buffer_size_changed;
 }
 
 void screen_read(struct frame_buffer *fb, bool alpha)
@@ -243,13 +242,13 @@ void screen_read(struct frame_buffer *fb, bool alpha)
     GLint vp[4];
     glGetIntegerv(GL_VIEWPORT, vp);
 
-    fb->width = vp[2];
+    fb->width = vp[2] & ~3;
     fb->height = vp[3];
     fb->pitch = fb->width;
 
     if (fb->pixels)
     {
-        glReadPixels(vp[0], vp[1], vp[2], vp[3], alpha ? GL_RGBA : GL_RGB, TEX_TYPE, fb->pixels);
+        glReadPixels(vp[0], vp[1], fb->width, fb->height, alpha ? GL_RGBA : GL_RGB, TEX_TYPE, fb->pixels);
     }
 }
 
@@ -331,6 +330,12 @@ void gl_screen_close(void)
     glDeleteBuffers(1, &buffer);
     glDeleteProgram(program);
 }
+
+uint8_t* screen_get_texture_data()
+{
+    return buffer_data + (rotate_buffer * buffer_size);
+}
+
 void screen_init()
 {
     memset(tex_width, 0, sizeof(int32_t) * TEX_NUM);
@@ -385,16 +390,14 @@ void screen_init()
 
     int32_t win_pf = ChoosePixelFormat(dc, &win_pfd);
     if (!win_pf) {
-  msg_error("Can't choose pixel format.");
-
+        msg_error("Can't choose pixel format.");
     }
     SetPixelFormat(dc, win_pf, &win_pfd);
 
     // create legacy context, required for wglGetProcAddress to work properly
     glrc = wglCreateContext(dc);
     if (!glrc || !wglMakeCurrent(dc, glrc)) {
-  msg_error("Can't create OpenGL context.");
-
+        msg_error("Can't create OpenGL context.");
     }
 
     // load wgl extension
@@ -413,8 +416,7 @@ void screen_init()
     if (!glrc_core || !wglMakeCurrent(dc, glrc_core)) {
         // rendering probably still works with the legacy context, so just send
         // a warning
-            msg_warning("Can't create OpenGL 3.3 core context.");
-
+        msg_warning("Can't create OpenGL 3.3 core context.");
     }
 
     // enable vsync
@@ -486,11 +488,6 @@ void screen_swap(bool blank)
 
    SwapBuffers(dc);
 
-}
-
-uint8_t* screen_get_texture_data()
-{
-    return buffer_data + (rotate_buffer * buffer_size);
 }
 
 void screen_toggle_fullscreen()

src/glguts.h
@@ -26,7 +26,7 @@ extern "C"
         bool valid;
     };
 
-    bool screen_write(struct frame_buffer *fb);
+    void screen_write(struct frame_buffer *fb);
     void screen_read(struct frame_buffer *fb, bool alpha);
     void screen_init();
     void screen_update(void);